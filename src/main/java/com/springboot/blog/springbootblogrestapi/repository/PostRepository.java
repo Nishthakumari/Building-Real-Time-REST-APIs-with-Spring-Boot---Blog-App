2129bf11e174c9e2b037a66b0be708d77158f92b
Nishthakumari <knishtha0@gmail.com>
2023-04-19 18:28:59 +0530

    Creating JPA Entity - Post Entity and JPA Repository - PostRepository

---
+++ src/main/java/com/springboot/blog/springbootblogrestapi/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.springboot.blog.springbootblogrestapi.repository;
+
+import com.springboot.blog.springbootblogrestapi.controller.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    
+
+}
