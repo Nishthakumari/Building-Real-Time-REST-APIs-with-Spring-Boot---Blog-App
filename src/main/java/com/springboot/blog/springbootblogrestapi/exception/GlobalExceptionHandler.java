f7c5c7c25cb92d68c9314d01eb7f1bebe28e3c81
Nishthakumari <knishtha0@gmail.com>
2023-04-20 17:02:42 +0530

    Spring Boot REST API Specific or Custom Exception Handling

---
+++ src/main/java/com/springboot/blog/springbootblogrestapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.springboot.blog.springbootblogrestapi.exception;
+
+import com.springboot.blog.springbootblogrestapi.payload.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest)
+    {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BlogAPIException.class)
+    public ResponseEntity<ErrorDetails> handleBlogNotFoundException(BlogAPIException exception, WebRequest webRequest)
+    {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+}
