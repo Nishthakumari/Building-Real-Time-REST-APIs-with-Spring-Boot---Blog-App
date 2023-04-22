52c7bc05efd6c3d9e51bc82a2d98b206f580a188
Nishthakumari <knishtha0@gmail.com>
2023-04-22 09:53:40 +0530

    Validate Create Post and Update Post REST API Request

--- src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/controller/PostController.java
@@ -4,6 +4,7 @@ import com.springboot.blog.springbootblogrestapi.payload.PostDto;
 import com.springboot.blog.springbootblogrestapi.payload.PostResponse;
 import com.springboot.blog.springbootblogrestapi.service.PostService;
 import com.springboot.blog.springbootblogrestapi.utils.AppConstants;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,7 @@ public class PostController {
 
     // create blog post rest api
     @PostMapping
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
+    public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
@@ -43,7 +44,7 @@ public class PostController {
 
     // update post by id rest api
     @PutMapping("/{id}")
-    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable(name = "id") long id){
+    public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id){
 
         PostDto postResponse = postService.updatePost(postDto, id);
 
--- src/main/java/com/springboot/blog/springbootblogrestapi/payload/PostDto.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/payload/PostDto.java
@@ -1,5 +1,7 @@
 package com.springboot.blog.springbootblogrestapi.payload;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.Set;
@@ -8,8 +10,16 @@ import java.util.Set;
 public class PostDto {
 
     private long id;
+
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least 2 characters.")
     private String title;
+
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 10 characters.")
     private String description;
+
+    @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 
