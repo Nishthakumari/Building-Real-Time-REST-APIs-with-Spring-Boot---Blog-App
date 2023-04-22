7909cfedcfe85e8aff39e83d13c65e53a4fce94e
Nishthakumari <knishtha0@gmail.com>
2023-04-22 10:29:37 +0530

    Customizing Validation Response

--- src/main/java/com/springboot/blog/springbootblogrestapi/payload/PostDto.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/payload/PostDto.java
@@ -15,6 +15,7 @@ public class PostDto {
     @Size(min = 2, message = "Post title should have at least 2 characters.")
     private String title;
 
+
     @NotEmpty
     @Size(min = 10, message = "Post description should have at least 10 characters.")
     private String description;
