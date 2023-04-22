acbdb400ca9ee695a17547d802cc40d8d1861452
Nishthakumari <knishtha0@gmail.com>
2023-04-22 10:42:33 +0530

    Validate Create Comment and Update Comment REST API Request

--- src/main/java/com/springboot/blog/springbootblogrestapi/controller/CommentController.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.springboot.blog.springbootblogrestapi.controller;
 
 import com.springboot.blog.springbootblogrestapi.payload.CommentDto;
 import com.springboot.blog.springbootblogrestapi.service.CommentService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,8 +20,8 @@ public class CommentController {
     }
 
     @PostMapping("/posts/{postId}/comments")
-    public ResponseEntity<CommentDto> createComment(@PathVariable(value = "postId") long postId,
-                                                    @RequestBody CommentDto commentDto){
+    public ResponseEntity<CommentDto> createComment( @PathVariable(value = "postId") long postId,
+                                                    @Valid @RequestBody CommentDto commentDto){
         return new ResponseEntity<>(commentService.createComment(postId, commentDto), HttpStatus.CREATED);
     }
 
@@ -39,7 +40,7 @@ public class CommentController {
     @PutMapping("/posts/{postId}/comments/{id}")
     public ResponseEntity<CommentDto> updateComment(@PathVariable(value = "postId") Long postId,
                                                     @PathVariable(value = "id") Long commentId,
-                                                    @RequestBody CommentDto commentDto){
+                                                    @Valid @RequestBody CommentDto commentDto){
         CommentDto updatedComment = commentService.updateComment(postId, commentId, commentDto);
         return new ResponseEntity<>(updatedComment, HttpStatus.OK);
     }
--- src/main/java/com/springboot/blog/springbootblogrestapi/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/payload/CommentDto.java
@@ -1,12 +1,23 @@
 package com.springboot.blog.springbootblogrestapi.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDto {
 
     private long id;
+
+    @NotEmpty(message = "Name should not be null or Empty")
     private String name;
+
+    @NotEmpty(message = "Name should not be null or Empty")
+    @Email
     private String email;
+    
+    @NotEmpty
+    @Size(min = 10, message = "Comment  body must be minimum 10 characters")
     private String body;
 }
