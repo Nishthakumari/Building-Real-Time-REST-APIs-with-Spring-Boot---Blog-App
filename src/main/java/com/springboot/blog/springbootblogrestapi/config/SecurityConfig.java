001af5f3a52f4ee12cd498783337c7042b2f646c
Nishthakumari <knishtha0@gmail.com>
2023-04-23 18:14:44 +0530

    Build Login REST API

--- src/main/java/com/springboot/blog/springbootblogrestapi/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -43,7 +42,8 @@ public class SecurityConfig {
                         //authorize.anyRequest().authenticated()
                         authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
                                 .requestMatchers("/api/auth/**").permitAll()
-                                .anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
+
+                                .anyRequest().authenticated());
 
 
 
