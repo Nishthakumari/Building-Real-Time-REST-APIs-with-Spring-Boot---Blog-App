75941a5f1b6ecc093a3d9e7dd67aa4d6d9114c39
Nishthakumari <knishtha0@gmail.com>
2023-04-24 18:33:14 +0530

    Adding SpringDoc OpenAPI to Spring Boot Project

--- src/main/java/com/springboot/blog/springbootblogrestapi/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/config/SecurityConfig.java
@@ -52,6 +52,8 @@ public class SecurityConfig {
                         //authorize.anyRequest().authenticated()
                         authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll()
                                 .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/swagger-ui/**").permitAll()
+                                .requestMatchers("/v3/api-docs/**").permitAll()
 
                                 .anyRequest().authenticated())
                 .exceptionHandling(exception -> exception
